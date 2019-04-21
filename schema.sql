654035e7a0fb4cdf66b1b57e4827d562f06442f6
alavaladeepika <99alavaladeepika@gmail.com>
2019-04-21 20:57:00 +0530

    Schema Update

--- schema.sql
+++ schema.sql
@@ -183,6 +183,7 @@ USE `WishlistService`;
 INSERT INTO `WishlistService`.`Login` (`login_id`, `password`) VALUES ('deepika', '12345');
 INSERT INTO `WishlistService`.`Login` (`login_id`, `password`) VALUES ('manisha', '12345');
 INSERT INTO `WishlistService`.`Login` (`login_id`, `password`) VALUES ('vaishali', '12345');
+INSERT INTO `WishlistService`.`Login` (`login_id`, `password`) VALUES ('suraj', '12345');
 
 COMMIT;
 
@@ -192,9 +193,10 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `WishlistService`;
-INSERT INTO `WishlistService`.`Customer` (`id`, `login_id`, `email_id`, `phone_no`, `dob`, `gender`, `name`) VALUES (1, 'deepika', 'deepika@gmail.com', '9611529722', '1997-10-25', 'F', 'Deepika Alavala');
+INSERT INTO `WishlistService`.`Customer` (`id`, `login_id`, `email_id`, `phone_no`, `dob`, `gender`, `name`) VALUES (1, 'deepika', '99alavaladeepika@gmail.com', '9611529722', '1997-10-25', 'F', 'Deepika Alavala');
 INSERT INTO `WishlistService`.`Customer` (`id`, `login_id`, `email_id`, `phone_no`, `dob`, `gender`, `name`) VALUES (2, 'manisha', 'manisha@gmail.com', '7730069061', '1996-06-11', 'F', 'Manisha Sinha');
 INSERT INTO `WishlistService`.`Customer` (`id`, `login_id`, `email_id`, `phone_no`, `dob`, `gender`, `name`) VALUES (3, 'vaishali', 'vaishali@gmail.com', '6301319864', '1996-03-09', 'F', 'Vaishali Walia');
+INSERT INTO `WishlistService`.`Customer` (`id`, `login_id`, `email_id`, `phone_no`, `dob`, `gender`, `name`) VALUES (4, 'suraj', 'surajjumpy@gmail.com', '9849813768', '1992-03-18', 'M', 'Suraj Singh');
 
 COMMIT;
 
@@ -204,14 +206,48 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `WishlistService`;
-INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (1, 'Book', 'Oxford', 'English Dictionary', 200, 12, 'images/catalog/1.jpeg');
-INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (2, 'Pen', 'Classmate', 'Blue pen', 40, 20, 'images/catalog/2.jpeg');
-INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (3, 'Book', 'Oxford', 'English Dictionary', 200, 12, 'images/catalog/1.jpeg');
-INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (4, 'Pen', 'Classmate', 'Blue pen', 40, 20, 'images/catalog/2.jpeg');
-INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (5, 'Book', 'Oxford', 'English Dictionary', 200, 12, 'images/catalog/1.jpeg');
-INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (6, 'Pen', 'Classmate', 'Blue pen', 40, 20, 'images/catalog/2.jpeg');
-INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (7, 'Book', 'Oxford', 'English Dictionary', 200, 12, 'images/catalog/1.jpeg');
-INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (8, 'Pen', 'Classmate', 'Blue pen', 40, 20, 'images/catalog/2.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (1, 'Women Party Wear', 'Biba', 'Nice dress for parties', 2000, 12, 'images/catalog/1.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (2, 'Women Casual Wear', 'W', 'Cotton cloth', 800, 12, 'images/catalog/2.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (3, 'Samsung UHD TV', 'Samsung', '42-inch TV', 20000, 12, 'images/catalog/3.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (4, 'Men Jeans', 'Denim', 'Blue jeans', 1200, 12, 'images/catalog/4.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (5, 'Women Jeans', 'Lee', 'Black Jeans', 1300, 12, 'images/catalog/5.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (6, 'Men Formal Wear', 'Arrow', 'White formal shirt', 1400, 12, 'images/catalog/6.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (7, 'Men Casual Wear', 'Indian Terrain', 'Red and Black Stripes', 1500, 12, 'images/catalog/7.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (8, 'Sony LED TV', 'Sony', '36-inch Tv', 12000, 12, 'images/catalog/8.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (9, 'Women Party Watch', 'Titan Raga', 'Gold finishing', 5000, 12, 'images/catalog/9.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (10, 'Women Band Watch', 'Fastrack', 'Red and Black band', 3000, 12, 'images/catalog/10.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (11, 'Men Band Watch', 'Fastrack', 'Black Band', 3500, 12, 'images/catalog/11.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (12, 'Men Formal Watch', 'Skagen', 'Grey Strap', 8000, 12, 'images/catalog/12.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (13, 'Women Walking Shoes', 'Adidas', 'Size 40 available', 5999, 12, 'images/catalog/13.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (14, 'Women Casual Footwear', 'Catwalk', 'Gold color', 1200, 12, 'images/catalog/14.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (15, 'Women Flipflops', 'Flipside', 'Yellow color', 300, 12, 'images/catalog/15.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (16, 'Men flipflops', 'Puma', 'Black color', 700, 12, 'images/catalog/16.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (17, 'Men Formal Shoes', 'Nike', 'Brown shoes', 3000, 12, 'images/catalog/17.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (18, 'Men Casual Footwear', 'Clarks', 'Tan color available', 1200, 12, 'images/catalog/18.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (19, 'Lakme Face cream', 'Lakme', 'For fairness', 300, 12, 'images/catalog/19.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (20, 'Fair and Handsome', 'Emami', 'For men fairness', 120, 12, 'images/catalog/20.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (21, 'Dinner set', 'Laopala', '12 pieces in box', 3000, 12, 'images/catalog/21.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (22, 'Induction stove', 'Panasonic', 'Warranty for 2 years', 3000, 12, 'images/catalog/22.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (23, 'Stove', 'Haffle', '5 slots available', 20000, 12, 'images/catalog/23.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (24, 'Toaster', 'Prestige', 'Bread toaster', 4000, 12, 'images/catalog/24.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (25, 'Stationary Kit', 'Apsara', 'All stationary available', 400, 12, 'images/catalog/25.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (26, 'Alarm Clock', 'Casio', 'Snooze option available', 200, 12, 'images/catalog/26.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (27, '4 set books', 'Classmate', 'Ruled books', 280, 12, 'images/catalog/27.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (28, 'Pen stand', 'Chumbak', 'Fancy pen stand', 400, 12, 'images/catalog/28.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (29, 'Cello Pens', 'Cello', '12 pens in the box', 120, 12, 'images/catalog/29.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (30, '12 step ladder', 'Bathla', '12 feet height', 4500, 12, 'images/catalog/30.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (31, 'Mini size cupboard', 'Godrej', 'Brown polishing', 1200, 12, 'images/catalog/31.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (32, 'Laptop table', 'Godrej', 'Wood table', 800, 12, 'images/catalog/32.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (33, 'Samsung A100', 'Samsung', '4GB RAM', 12000, 12, 'images/catalog/33.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (34, 'Oneplus 6t', 'Oneplus', 'Notch available', 39999, 12, 'images/catalog/34.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (35, 'iPhone XS', 'Apple', 'Long battery', 90000, 12, 'images/catalog/35.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (36, 'Ideapad 510', 'Lenovo', '8GB RAM ', 50000, 12, 'images/catalog/36.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (37, 'Inspiron 560', 'Dell', '1 TB Memory', 56000, 12, 'images/catalog/37.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (38, 'Mac Book Air', 'Apple', '256GB RAM', 99999, 12, 'images/catalog/38.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (39, 'Notebook 360', 'HP', 'Touch screen laptop', 23000, 12, 'images/catalog/39.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (40, 'Dobby wallpaper', 'Abstract', '36 cm long', 300, 12, 'images/catalog/40.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (41, 'Eifel tower wallpaper', 'Sathrangi', '46 inch height', 420, 12, 'images/catalog/41.jpeg');
+INSERT INTO `WishlistService`.`Catalog` (`product_id`, `product_name`, `brand`, `description`, `price`, `quantity`, `pic_location`) VALUES (42, 'Paper Clips', 'Apex', '20 clips in the box', 130, 12, 'images/catalog/42.jpeg');
 
 COMMIT;
 
